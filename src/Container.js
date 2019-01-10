eaa3e492811ab25a9aaeb11d7b638ffd5db8097c
Tristan Scaglia <tristan.scaglia@gmail.com>
2019-01-10 17:52:23 +0100

    Created app boilerplate

---
+++ src/Container.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+// Container holds the state of our app
+class Container extends Component {
+  render() {
+    return <div>Test</div>;
+  }
+}
+
+export default Container;
